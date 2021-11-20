af4946d58888f4902d6dfe6fd9e8d4d802c3af74
va1era4 <94139302+va1era4@users.noreply.github.com>
2021-11-20 15:07:22 +0200

    files

---
+++ 11111.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream> 
+#include <iomanip> 
+#include "windows.h"
+#include "math.h"
+
+using namespace std;
+
+int main()
+{
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+    int x, y;
+    cout << "Введіть x = ";
+    cin >> x;
+    cout << "Введіть y = ";
+    cin >> y;
+    float rez, rez1;
+    int s = 0;
+    for (int k = 1; k < 6; k++)
+    {
+        s = s + x*k;*//зміна коду
+    }
+    rez = pow(x, 3) + pow(sin(x), 2) - 3 / s + pow(x, 4);
+    cout << "Y=" << rez << endl;
+    int d = 0;
+    for (int m = 1; m < 6; m++)
+    {
+        d = d * m;
+    }
+    rez1 = tan(d) + pow(y, 5);
+    cout << "Z=" << rez1 << endl;
+    system("pause");
+    return 0;
+}
